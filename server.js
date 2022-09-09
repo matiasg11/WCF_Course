c5a7afb62afef7a2d33e2b3e12cfb32b06653c5d
Matias Guarnera <matiasguarnera@gmail.com>
2022-09-09 15:41:49 -0300

    Learnt CRUD with Mosh

--- server.js
+++ server.js
@@ -1,47 +1,15 @@
+//First it will look for the module as a core module, then as a folder and finally inside node_modules
+
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const PORT = 8000
+const port = process.env.PORT || 8000
 
 app.use(cors())
 
 let rappers = {
-    '21 savage': {
-        'age': 28,
-        'birthName': 'Shéyaa Bin Abraham-Joseph',
-        'birthdate': '22 October 1992', 
-        'birthLocation': 'London, England',
-        'origin': 'Atlanta, Georgia',
-        'genre': 'hip hop, trap, rap, horrorcore',
-        'occupation': 'rapper, songwriter, record producer',
-        'yearsActive': '2013-present',
-        'labels': 'Epic, Slaughter Gang',
-        'children': 3
-    },
-    'chance the rapper':{
-        'age': 28,
-        'birthName': 'Chancelor Jonathan Bennett',
-        'birthdate': 'April 16, 1993', 
-        'birthLocation': 'London, England',
-        'origin': 'Chicago, Illinois',
-        'genre': 'hip hop, alternative hip hop, r & b',
-        'occupation': 'rapper, singer, song writer, record producer, activist, actor, philanthropist',
-        'yearsActive': '2011-present',
-        'labels': 'none',
-        'children': 0
-    },
-    'unknown':{
-        'age': 'unknown',
-        'birthName': 'unknown',
-        'birthdate': 'unknown', 
-        'birthLocation': 'unknown',
-        'origin': 'unknown',
-        'genre': 'unknown',
-        'occupation': 'unknown',
-        'yearsActive': 'unknown',
-        'labels': 'unknown',
-        'children': 'unknown'
-    }
+    
+    
 }
 
 app.get('/', (request, response) => {
@@ -50,6 +18,7 @@ app.get('/', (request, response) => {
 
 app.get('/api/:name', (request, response) => {
     const rapperName = request.params.name.toLowerCase()
+    //request.query is for optionals
     if(rappers[rapperName]){
         response.json(rappers[rapperName])
     }else{
@@ -57,6 +26,6 @@ app.get('/api/:name', (request, response) => {
     }
 })
 
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 })
