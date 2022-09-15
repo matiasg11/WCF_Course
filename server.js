9d278a7bd422ca42da3df255065c6408a163fdb3
Matias Guarnera <matiasguarnera@gmail.com>
2022-09-15 18:25:59 -0300

    Vidly finished for movie genres. CRUD acknowledged

--- server.js
+++ server.js
@@ -3,45 +3,82 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Joi = require('joi')
+const { response } = require('express')
 const port = process.env.PORT || 8000
 app.use(express.json())
 app.use(cors())
 
-let movies = [
-    {id:1, name:"movie1", genre:"Terror"},
-    {id:2, name:"movie2", genre:"Suspense"},
-    {id:3, name:"movie3", genre:"Comedy"},
-    {id:4, name:"movie4", genre:"Thriller"},
-    {id:5, name:"movie5", genre:"Terror"},
-    {id:6, name:"movie6", genre:"Comedy"},
+let movieGenres = [
+    {id:1, name:"Terror"},
+    {id:2, name:"Suspense"},
+    {id:3, name:"Comedy"},
+    {id:4, name:"Thriller"},
+    {id:5, name:"Comedy"},
 ]
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api/movies', (request, response) => {
-    response.send(movies)
+app.get('/api/movieGenres', (request, response) => {
+    response.send(movieGenres)
 })
 
-app.get('/api/movies/:genre', (request, response) => {
-    const movieGenre = movies.find(elem => elem.genre.toLowerCase() === request.params.genre.toLowerCase())
-    if (!movieGenre){
-        return response.status(404).send("The genre does not exist.")
+
+app.post('/api/movieGenres', (request, response)=>{
+    const {error} = validateGenre(request.body);
+    if (error) return response.status(400).send(error.details[0].message)
+
+
+    let movieGenre = {
+        id: movieGenres.length+1,
+        name: request.body.name
     }
 
+    movieGenres.push(movieGenre)
     response.send(movieGenre)
 })
 
-app.post('/api/movies', (request, response)=>{
-    let movie = {
-        id: `${movies.length+1}`,
-        name:`movie${movies.length+1}`,
-        genre: request.body.genre
-    }
-    movies.push(movie)
-    response.send(movie)
+app.put('/api/movieGenres/:id', (request, response)=> {
+    const movieGenre = movieGenres.find(item => item.id === parseInt(request.params.id))
+    if (!movieGenre) return response.status(404).send("The genre with the corresponding ID was not found. Sorry!")
+
+    const {error} = validateGenre(request.body)
+    if (error) return response.status(400).send(error.details[0].message)
+
+    movieGenre.name = request.body.name
+    response.send(movieGenre)
 })
+
+app.delete('/api/movieGenres/:id', (request, response)=> {
+    const movieGenre = movieGenres.find(item => item.id === parseInt(request.params.id))
+    if (!movieGenre) return response.status(400).send("The genre with the corresponding ID was not found. Sorry!")
+
+    const index = movieGenres.indexOf(movieGenre)
+    movieGenres.splice(index, 1)
+    response.send(movieGenre)
+})
+
+app.get('/api/movieGenres/:id', (request, response) => {
+    const movieGenre = movieGenres.find(item => item.id === parseInt(request.params.id))
+
+    if (!movieGenre){
+        return response.status(404).send("The genre with the corresponding ID was not found. Sorry!")
+    }
+    
+    response.send(movieGenre)
+})
+
+function validateGenre(genre){
+    const schema = Joi.object({ name: Joi.string().min(3).required()});
+    
+    let answer =  schema.validate(genre)
+    console.log(answer)
+    return answer
+    
+    
+}
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
